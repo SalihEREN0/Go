93b41236be73778ab41d35a3b2b7ae2af6893438
Salih Eren <saliheren266@gmail.com>
2025-01-27 18:59:50 +0300

    UDP multiple cilents

--- UDP/client/client.go
+++ UDP/client/client.go
@@ -5,46 +5,60 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"sync"
 )
 
 func main() {
-	serverAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:12345")
+	var wg sync.WaitGroup
+
+	for i := 0; i < 3; i++ {
+		wg.Add(1)
+		go func(clientID int) {
+			defer wg.Done()
+			runClient(clientID)
+		}(i)
+	}
+
+	wg.Wait()
+}
+
+func runClient(clientID int) {
+	serverAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:1234")
 	if err != nil {
-		fmt.Println("Error resolving server address:", err)
+		fmt.Printf("Client %d: Error resolving server address: %v\n", clientID, err)
 		return
 	}
 
 	conn, err := net.DialUDP("udp", nil, serverAddr)
 	if err != nil {
-		fmt.Println("Error connecting to server:", err)
+		fmt.Printf("Client %d: Error connecting to server: %v\n", clientID, err)
 		return
 	}
 	defer conn.Close()
 
-	message := 0
-
-	for message != 27 {
+	for {
 		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("Enter message to send: ")
+		fmt.Printf("Client %d: Enter message to send: ", clientID)
 
 		message, err := reader.ReadString('\n')
 		if err != nil {
-			fmt.Println("Error reading input:", err)
+			fmt.Printf("Client %d: Error reading input: %v\n", clientID, err)
 			return
 		}
+
 		_, err = conn.Write([]byte(message))
 		if err != nil {
-			fmt.Println("Error sending message:", err)
+			fmt.Printf("Client %d: Error sending message: %v\n", clientID, err)
 			return
 		}
 
 		buffer := make([]byte, 1024)
 		n, _, err := conn.ReadFromUDP(buffer)
 		if err != nil {
-			fmt.Println("Error reading response:", err)
+			fmt.Printf("Client %d: Error reading response: %v\n", clientID, err)
 			return
 		}
 
-		fmt.Printf("Response from server: %s\n", string(buffer[:n]))
+		fmt.Printf("Client %d: Response from server: %s\n", clientID, string(buffer[:n]))
 	}
 }
--- UDP/server.go
+++ UDP/server.go
@@ -4,64 +4,88 @@ import (
 	"log"
 	"net"
 	"sync"
-	"time"
 )
 
 type Server struct {
 	listenAddr string
 	conn       net.PacketConn
-	quich      chan struct{}
+	quit       chan struct{}
+	clients    map[string]net.Addr
+	mu         sync.Mutex
 }
 
 func newServer(addr string) *Server {
 	return &Server{
 		listenAddr: addr,
-		quich:      make(chan struct{}),
+		quit:       make(chan struct{}),
+		clients:    make(map[string]net.Addr),
 	}
 }
 
-func Start(s *Server, wg *sync.WaitGroup) error {
+func (s *Server) Start(wg *sync.WaitGroup) error {
 	defer wg.Done()
 
 	conn, err := net.ListenPacket("udp", s.listenAddr)
 	if err != nil {
 		return err
 	}
-	defer conn.Close()
 	s.conn = conn
+	defer s.conn.Close()
+
 	log.Printf("Server started on %s\n", s.listenAddr)
+	go s.listen()
 
-	go func() {
-		time.Sleep(10 * time.Second)
-		close(s.quich)
-	}()
-
-	<-s.quich
-	log.Printf("Server stopped on %s\n", s.listenAddr)
+	<-s.quit
 	return nil
+}
+
+func (s *Server) listen() {
+	buffer := make([]byte, 1024)
+	for {
+		n, addr, err := s.conn.ReadFrom(buffer)
+		if err != nil {
+			log.Println("Error reading from connection:", err)
+			return
+		}
+
+		s.addClient(addr)
+
+		log.Printf("Received %d bytes from %s: %s\n", n, addr, string(buffer[:n]))
+
+		s.broadcast(buffer[:n], addr)
+	}
+}
+
+func (s *Server) addClient(addr net.Addr) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if _, exists := s.clients[addr.String()]; !exists {
+		log.Printf("New client added: %s\n", addr)
+		s.clients[addr.String()] = addr
+	}
+}
+
+func (s *Server) broadcast(message []byte, sender net.Addr) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	for _, clientAddr := range s.clients {
+		if clientAddr.String() != sender.String() {
+			_, err := s.conn.WriteTo(message, clientAddr)
+			if err != nil {
+				log.Printf("Error broadcasting to %s: %v\n", clientAddr, err)
+			}
+		}
+	}
 }
 
 func main() {
 	var wg sync.WaitGroup
 
-	ser := newServer(":1234")
-	ser2 := newServer(":1235")
-	ser3 := newServer(":1236")
+	server := newServer(":1234")
 
-	wg.Add(3)
-
+	wg.Add(1)
 	go func() {
-		if err := Start(ser, &wg); err != nil {
-			log.Fatal(err)
-		}
-	}()
-	go func() {
-		if err := Start(ser2, &wg); err != nil {
-			log.Fatal(err)
-		}
-	}()
-	go func() {
-		if err := Start(ser3, &wg); err != nil {
+		if err := server.Start(&wg); err != nil {
 			log.Fatal(err)
 		}
 	}()
